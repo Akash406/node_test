0df70af5605a34843f0b6f4fa3ec2c2857cf2c16
ajayasha <ajayasha@visa.com>
2017-03-07 18:20:56 -0800

    commit messenger bot

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 var express = require('express');
+var bodyparser = require('body-parser');
+var request = require('request');
+
 var app = express();
 
 var port = process.env.PORT || 8080;
+app.set('port', port);
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    res.set('Hi I am chatbot ');
 });
+
+app.get('/webhook/', function(req, res)){
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "test_messengerbot") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+
+});
+
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
