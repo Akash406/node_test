eff893d2506c8260a5263fec3dd3b66505e0030f
ajayasha <ajayasha@visa.com>
2017-03-09 10:26:03 -0800

    template correction

--- index.js
+++ index.js
@@ -127,103 +127,103 @@ function showItenary(recipientId){
        ],
        flight_info: [
           {
-            "connection_id": "c001",
-            "segment_id": "s001",
-            "flight_number": "KL9123",
-            "aircraft_type": "Boeing 737",
-            "departure_airport": {
-              "airport_code": "SFO",
-              "city": "San Francisco",
-              "terminal": "T4",
-              "gate": "G8"
+            connection_id: "c001",
+            segment_id: "s001",
+            flight_number: "KL9123",
+            aircraft_type: "Boeing 737",
+            departure_airport: {
+              airport_code: "SFO",
+              city: "San Francisco",
+              terminal: "T4",
+              gate: "G8"
             },
             arrival_airport: {
-              "airport_code": "SLC",
-              "city": "Salt Lake City",
-              "terminal": "T4",
-              "gate": "G8"
+              airport_code: "SLC",
+              city: "Salt Lake City",
+              terminal: "T4",
+              gate: "G8"
             },
             flight_schedule: {
-              "departure_time": "2016-01-02T19:45",
-              "arrival_time": "2016-01-02T21:20"
+              departure_time: "2016-01-02T19:45",
+              arrival_time: "2016-01-02T21:20"
             },
-            "travel_class": "business"
+            travel_class: "business"
           },
           {
-            "connection_id": "c002",
-            "segment_id": "s002",
-            "flight_number": "KL321",
-            "aircraft_type": "Boeing 747-200",
-            "travel_class": "business",
-            "departure_airport": {
-              "airport_code": "SLC",
-              "city": "Salt Lake City",
-              "terminal": "T1",
-              "gate": "G33"
+            connection_id: "c002",
+            segment_id: "s002",
+            flight_number: "KL321",
+            aircraft_type: "Boeing 747-200",
+            travel_class: "business",
+            departure_airport: {
+              airport_code: "SLC",
+              city: "Salt Lake City",
+              terminal: "T1",
+              gate: "G33"
             },
             arrival_airport: {
-             "airport_code": "AMS",
-             "city": "Amsterdam",
-             "terminal": "T1",
-             "gate": "G33"
+             airport_code: "AMS",
+             city: "Amsterdam",
+             terminal: "T1",
+             gate: "G33"
            },
            flight_schedule: {
-             "departure_time": "2016-01-02T22:45",
-             "arrival_time": "2016-01-03T17:20"
+             departure_time: "2016-01-02T22:45",
+             arrival_time: "2016-01-03T17:20"
            }
          }
        ],
        passenger_segment_info: [
          {
-           "segment_id": "s001",
-           "passenger_id": "p001",
-           "seat": "12A",
-           "seat_type": "Business"
+           segment_id: "s001",
+           passenger_id: "p001",
+           seat: "12A",
+           seat_type: "Business"
          },
          {
-           "segment_id": "s001",
-           "passenger_id": "p002",
-           "seat": "12B",
-           "seat_type": "Business"
+           segment_id: "s001",
+           passenger_id: "p002",
+           seat: "12B",
+           seat_type: "Business"
          },
          {
-           "segment_id": "s002",
-           "passenger_id": "p001",
-           "seat": "73A",
-           "seat_type": "World Business",
+           segment_id: "s002",
+           passenger_id: "p001",
+           seat: "73A",
+           seat_type: "World Business",
            product_info: [
              {
-               "title": "Lounge",
-               "value": "Complimentary lounge access"
+               title: "Lounge",
+               value: "Complimentary lounge access"
              },
              {
-               "title": "Baggage",
-               "value": "1 extra bag 50lbs"
+               title: "Baggage",
+               value: "1 extra bag 50lbs"
              }
            ]
          },
          {
-           "segment_id": "s002",
-           "passenger_id": "p002",
-           "seat": "73B",
-           "seat_type": "World Business",
+           segment_id: "s002",
+           passenger_id: "p002",
+           seat: "73B",
+           seat_type: "World Business",
            product_info: [
              {
-               "title": "Lounge",
-               "value": "Complimentary lounge access"
+               title: "Lounge",
+               value: "Complimentary lounge access"
              },
              {
-               "title": "Baggage",
-               "value": "1 extra bag 50lbs"
+               title: "Baggage",
+               value: "1 extra bag 50lbs"
              }
            ]
          }
        ],
        price_info: [
          {
-           "title": "Fuel surcharge",
-           "amount": "1597",
-           "currency": "USD"
+           title: "Fuel surcharge",
+           amount: "1597",
+           currency: "USD"
          }
        ]
      }
