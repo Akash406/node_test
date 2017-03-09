ac9072104ad9f3fc236199986a36318d7d74d811
ajayasha <ajayasha@visa.com>
2017-03-09 11:29:36 -0800

    update payload

--- index.js
+++ index.js
@@ -93,145 +93,38 @@ function showItenary(recipientId){
     recipient:{
       id: recipientId
 
-    },message:{
-      attachment: {
-        type : "template",
-        payload : {
-        template_type: "airline_itinerary",
-        intro_message: "Here\'s your flight itinerary.",
+    },message: {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "airline_update",
+        intro_message: "Your flight is delayed",
+        update_type: "delay",
         locale: "en_US",
-        pnr_number: "ABCDEF",
-        passenger_info: [
-         {
-           name: "Farbound Smith Jr",
-           ticket_number: "0741234567890",
-           passenger_id: "p001"
-         },
-         {
-           name: "Nick Jones",
-           ticket_number: "0741234567891",
-           passenger_id: "p002"
-         }
-       ],
-       passenger_info: [
-         {
-           name: "Farbound Smith Jr",
-           ticket_number: "0741234567890",
-           passenger_id: "p001"
-         },
-         {
-           name: "Nick Jones",
-           ticket_number: "0741234567891",
-           passenger_id: "p002"
-         }
-       ],
-       flight_info: [
-          {
-            connection_id: "c001",
-            segment_id: "s001",
-            flight_number: "KL9123",
-            aircraft_type: "Boeing 737",
-            departure_airport: {
-              airport_code: "SFO",
-              city: "San Francisco",
-              terminal: "T4",
-              gate: "G8"
-            },
-            arrival_airport: {
-              airport_code: "SLC",
-              city: "Salt Lake City",
-              terminal: "T4",
-              gate: "G8"
-            },
-            flight_schedule: {
-              departure_time: "2016-01-02T19:45",
-              arrival_time: "2016-01-02T21:20"
-            },
-            travel_class: "business"
+        pnr_number: "CF23G2",
+        update_flight_info: {
+          flight_number: "KL123",
+          departure_airport: {
+            airport_code: "SFO",
+            city: "San Francisco",
+            terminal: "T4",
+            gate: "G8"
           },
-          {
-            connection_id: "c002",
-            segment_id: "s002",
-            flight_number: "KL321",
-            aircraft_type: "Boeing 747-200",
-            travel_class: "business",
-            departure_airport: {
-              airport_code: "SLC",
-              city: "Salt Lake City",
-              terminal: "T1",
-              gate: "G33"
-            },
-            arrival_airport: {
-             airport_code: "AMS",
-             city: "Amsterdam",
-             terminal: "T1",
-             gate: "G33"
-           },
-           flight_schedule: {
-             departure_time: "2016-01-02T22:45",
-             arrival_time: "2016-01-03T17:20"
-           }
-         }
-       ],
-       passenger_segment_info: [
-         {
-           segment_id: "s001",
-           passenger_id: "p001",
-           seat: "12A",
-           seat_type: "Business"
-         },
-         {
-           segment_id: "s001",
-           passenger_id: "p002",
-           seat: "12B",
-           seat_type: "Business"
-         },
-         {
-           segment_id: "s002",
-           passenger_id: "p001",
-           seat: "73A",
-           seat_type: "World Business",
-           product_info: [
-             {
-               title: "Lounge",
-               value: "Complimentary lounge access"
-             },
-             {
-               title: "Baggage",
-               value: "1 extra bag 50lbs"
-             }
-           ]
-         },
-         {
-           segment_id: "s002",
-           passenger_id: "p002",
-           seat: "73B",
-           seat_type: "World Business",
-           product_info: [
-             {
-               title: "Lounge",
-               value: "Complimentary lounge access"
-             },
-             {
-               title: "Baggage",
-               value: "1 extra bag 50lbs"
-             }
-           ]
-         }
-       ],
-       price_info: [
-         {
-           title: "Fuel surcharge",
-           amount: "1597",
-           currency: "USD"
-         }
-       ]
-     }
-
+          arrival_airport: {
+            airport_code: "AMS",
+            city: "Amsterdam",
+            terminal: "T4",
+            gate: "G8"
+          },
+          flight_schedule: {
+            boarding_time: "2015-12-26T10:30",
+            departure_time: "2015-12-26T11:30",
+            arrival_time: "2015-12-27T07:30"
+          }
+        }
       }
-
-
     }
+  }
 
   };
   callSendAPI(messageData)
